c190e53d1eeb0641b479c4845c44fc7b67b07686
TheRoy09 <167314973+TheRoy09@users.noreply.github.com>
2024-06-15 11:49:14 +0530

    Prepare for deployment

--- gwen/app/__init__.py
+++ gwen/app/__init__.py
@@ -1,39 +1,42 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask_mail import Mail
+from dotenv import load_dotenv
+import os
 
-# Initialize extensions
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+# Initialize the scheduler
 scheduler = BackgroundScheduler()
-mail = Mail()
+scheduler.start()
+
+from app import routes, tasks
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
-    # Initialize extensions
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+
+    scheduler = BackgroundScheduler()
     scheduler.start()
-    mail.init_app(app)
 
-    login_manager.login_view = 'login'
-    login_manager.login_message_category = 'info'
-
-    # Import routes and tasks
     from app import routes, tasks
-
     return app
 
 app = create_app()
-
-# Create the database tables if they don't exist yet
-with app.app_context():
-    db.create_all()
